d8eb5b7699647240cfa4d5c3914b23fc5f13cef0
kodehode <kodehode@gmail.com>
2022-04-13 10:50:54 +0200

    Fixed typo.

--- components/AuthorIntro.js
+++ components/AuthorIntro.js
@@ -18,7 +18,7 @@ const AuthorIntro = () =>
                 <h5 className="font-weight-bold mb-0">Hello,</h5>
                 <p className="welcome-text">
                 My name is Sjur. I am a project/product manager and general technology enthusiast.
-                And this is a blog app I built to explore ract/next/sanity. Not to blog.
+                And this is a blog app I built to explore react/next/sanity. Not to blog.
                 </p>
             </Media.Body>
             </Media>
