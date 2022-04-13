7cdc23390d2395da9dd1403e55d39d8002132e81
kodehode <kodehode@gmail.com>
2022-04-13 10:04:41 +0200

    Bumped next.js to 12. Changed author intro text.

--- components/AuthorIntro.js
+++ components/AuthorIntro.js
@@ -17,8 +17,8 @@ const AuthorIntro = () =>
             <Media.Body>
                 <h5 className="font-weight-bold mb-0">Hello,</h5>
                 <p className="welcome-text">
-                My name is Sjur and I am an experienced project/product manager and general technology enthusiast.
-                And this is my blog.
+                My name is Sjur. I'm a project/product manager and general technology enthusiast.
+                And this is a blog app I built to explore ract/next/sanity. Not to blog.
                 </p>
             </Media.Body>
             </Media>
