7fabbbb681400ab146295934e8273963dbe9bbaf
kodehode <kodehode@gmail.com>
2022-09-24 09:24:26 +0200

    Fix typo
    Fix typo

--- components/AuthorIntro.js
+++ components/AuthorIntro.js
@@ -17,7 +17,7 @@ const AuthorIntro = () =>
             <Media.Body>
                 <h5 className="font-weight-bold mb-0">Hello,</h5>
                 <p className="welcome-text">
-            my name is Sjur. I&apos;m a interested in product development, technology, gadgets, food, science... well, lots of stuff.
+            my name is Sjur. I&apos;m interested in product development, technology, gadgets, food, science... well, lots of stuff.
                 And this is a blog app I built to explore react/next/sanity. Not to blog.
                 </p>
             </Media.Body>
